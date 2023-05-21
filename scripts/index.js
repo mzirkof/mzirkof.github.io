c37d8b74c9529d1aaf10a4309179573952b9fd53
Celestin SOUOP <souop_007@yahoo.fr>
2023-05-21 11:04:31 -0700

    added and updated javascript file to add interactivity with hamburger logo

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,22 @@
 console.log('set');
 
 let btn_menu = document.getElementById('menu_icon');
+let btn_close_menu = document.getElementById('close_menu_icon');
+
+let toolbar = document.getElementById('toolbar');
+let mobile_menu = document.getElementById('mobile_menu');
+
 
 // btn_menu.style.display='none';
 let i=0;
 btn_menu.addEventListener('click', ( ) =>{
+    // mobile_menu.style.display='flex';
+    mobile_menu.className='mobile_menu d_flex';
     i++;
-    console.log('has clicke '+i+' times')
+    console.log('has been clicked '+i+' times')
+})
+
+btn_close_menu.addEventListener('click', ( ) =>{
+    mobile_menu.className='d_none';
 })
 // console.log(btn_menu);
