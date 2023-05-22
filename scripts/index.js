9affc90b18f371cc0e1dbfad7be9f5bd6172fcd9
Celestin SOUOP <souop_007@yahoo.fr>
2023-05-22 09:26:01 -0700

    updated index js file to fix linter errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,21 @@
-console.log('set');
+let i = 0;
+// console.log('set');
 
-let btn_menu = document.getElementById('menu_icon');
-let btn_close_menu = document.getElementById('close_menu_icon');
+const btnMenu = document.getElementById('menu_icon');
+const btnCloseMenu = document.getElementById('close_menu_icon');
 
-let toolbar = document.getElementById('toolbar');
-let mobile_menu = document.getElementById('mobile_menu');
-
+// const toolbar = document.getElementById('toolbar');
+const mobileMenu = document.getElementById('mobile_menu');
 
 // btn_menu.style.display='none';
-let i=0;
-btn_menu.addEventListener('click', ( ) =>{
-    // mobile_menu.style.display='flex';
-    mobile_menu.className='mobile_menu d_flex';
-    i++;
-    console.log('has been clicked '+i+' times')
-})
+btnMenu.addEventListener('click', () => {
+  // mobile_menu.style.display='flex';
+  mobileMenu.className = `mobile_menu d_flex ${i}`;
+  i += 1;
+//   console.log(`has been clicked ${i} times`);
+});
 
-btn_close_menu.addEventListener('click', ( ) =>{
-    mobile_menu.className='d_none';
-})
+btnCloseMenu.addEventListener('click', () => {
+  mobileMenu.className = 'd_none';
+});
 // console.log(btn_menu);
