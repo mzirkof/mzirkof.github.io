24e86659a4d574bde385a1773e43ff79990820bb
Celestin SOUOP <souop_007@yahoo.fr>
2024-05-18 08:15:29 -0700

    Fixing Linters Errors
    Troubles founds  in synthax by eslint and stylelint have been fixed up during theses steps

--- scripts/Windows/ProjectsDone/TicTacToe/js/tictactoeGame.js
+++ scripts/Windows/ProjectsDone/TicTacToe/js/tictactoeGame.js
@@ -1,18 +1,5 @@
 // const gameZone = document.getElementById('mainTttGame')
 let gameZone = document.getElementById('mainTttGame');
-const btnWindow = document.getElementById('p1').addEventListener('click', () => {
-  goGame();
-  // setTimeout(() => {
-  //     goGame()
-  // }, 2000);
-  function goGame(params) {
-    gameZone = document.getElementById('mainTttGame');
-    drawCanvas();
-    // setEventListener()
-    refreshCanvas();
-    // console.log(gameZone)
-  }
-});
 
 const playerOne = 1;
 const playerTwo = 2;
@@ -29,24 +16,29 @@ const matrix = t;
 // console.log(gameZone)
 
 // ===================Function that initialize the matrix of the Game =====================
-function initMatrix(size) {
-  const init = [0];
-  let i = 1;
-  while (i < size) {
-    init.push(0);
-    i++;
-  }
+// function initMatrix(size) {
+//   const init = [0];
+//   let i = 1;
+//   while (i < size) {
+//     init.push(0);
+//     i += 1;
+//   }
 
-  i = 1;
-  const matrix = [init];
-  while (i < size) {
-    matrix.push(init);
-    i++;
-  }
-  return matrix;
-}
+//   i = 1;
+//   const matrix = [init];
+//   while (i < size) {
+//     matrix.push(init);
+//     i += 1;
+//   }
+//   return matrix;
+// }
 
-// Function that control the Game result (Is there a winner or is it a Draw Game Or the Game is steel going on ?)
+// const matrix 2=initMatrix(3);
+
+/**
+ * Function that control the Game result
+ * (Is there a winner or is it a Draw Game Or the Game is steel going on ?)
+ */
 function checkGame(matrix) {
   function isSame(matrix, direction) {
     /**  The logic here is to check tha matrix of the game on every possible winning direction
@@ -58,7 +50,8 @@ function checkGame(matrix) {
          *              4   for the other diagonal line (x = y)
          * @returns - an Object result containing :
          *              winned : which is a boolean telling if someone has won the game (true value)
-         *              winner : which give the val of the one who has won ( player1 or player2 ) Notice that when the winner is steel null it means that the values was 0.
+         *              winner : which give the val of the one who has won ( player1 or player2 )
+         *               Notice that when the winner is steel null it means that the values was 0.
         */
 
     const result = {
@@ -83,7 +76,7 @@ function checkGame(matrix) {
               flag = false;
               break;
             }
-            j++;
+            j += 1;
           }
           if (flag) {
             // if flag is steel true it means that there is a Winner
@@ -92,7 +85,7 @@ function checkGame(matrix) {
             // console.log('Victoire en ligne '+i)
             break;
           } else {
-            i++;
+            i += 1;
             j = 0;
           }
         }
@@ -110,7 +103,7 @@ function checkGame(matrix) {
               flag = false;
               break;
             }
-            i++;
+            i += 1;
           }
 
           if (flag) {
@@ -120,7 +113,7 @@ function checkGame(matrix) {
             // console.log('Victoire a la colonne : '+j)
             break;
           }
-          j++;
+          j += 1;
         }
         break;
       }
@@ -134,7 +127,7 @@ function checkGame(matrix) {
             flag = false;
             break;
           }
-          i++;
+          i += 1;
         }
 
         if (flag) {
@@ -161,10 +154,10 @@ function checkGame(matrix) {
                 break;
               }
             }
-            j++;
+            j += 1;
           }
 
-          i++;
+          i += 1;
         }
 
         if (flag) {
@@ -189,7 +182,7 @@ function checkGame(matrix) {
     return result;
   }
   const check = [];
-  for (let index = 0; index < 4; index++) {
+  for (let index = 0; index < 4; index += 1) {
     check.push(isSame(matrix, index + 1));
   }
 
@@ -197,144 +190,19 @@ function checkGame(matrix) {
 }
 
 function resetMatrix() {
-  for (let i = 0; i < matrix.length; i++) {
+  for (let i = 0; i < matrix.length; i += 1) {
     // const element = matrix[i];
-    for (let j = 0; j < matrix[i].length; j++) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
       // const item = matrix[i][j];
       matrix[i][j] = 0;
     }
   }
   // console.log(matrix)
 }
-
-function setMatrixValue(coordinates, value) {
-
-  // console.log(matrix)
-  // if ((coordinates.line<matrix.length && coordinates.line>=0) && (coordinates.column<matrix.length && coordinates.column>=0) ) {
-  //     // matrix[coordinates.line][coordinates.column] = value
-  //     // matrix[coordinates.line][coordinates.column] = value
-  //     matrix[0][1]=5
-  //     console.log(matrix)
-  // } else {
-  //     const n = matrix.length - 1
-  //     console.log('wrong coordinates : line and column must be numbers between 0 and '+n+' both included and not ('+coordinates.line+';'+coordinates.column+')')
-  // }
-
-}
-
-function setEventListener() {
-  const places = [...document.getElementsByClassName('place')];
-  places.forEach((element) => {
-    element.addEventListener('click', () => {
-      if (gameResult.winned) {
-        console.log('yes');
-      } else if (roundNumber > size + 1) {
-        console.log('no');
-      } else {
-        if (roundNumber === size + 1) roundNumber++;
-        if (!gameStarted) {
-          gameStarted = true;
-        }
-        playEvent(element);
-        // console.log('The Matrix now is : '+ matrix)
-
-        const result = checkGame(matrix);
-        for (let index = 0; index < result.length; index++) {
-          const element = result[index];
-          if (element.winned) {
-            gameResult = element;
-            break;
-          }
-        }
-
-        gameDecision(gameResult, roundNumber);
-      }
-    });
-  });
-
-  const resetBtn = document.getElementById('resetGame');
-  resetBtn.addEventListener('click', () => {
-    if (gameStarted) {
-      // Reset The matrix and set game started to false
-      console.log('resetting the game');
-      // matrix = [[0,0,0],[0,0,0],[0,0,0]]
-      resetMatrix();
-      // console.log('matrix is '+matrix)
-
-      const places = [...document.getElementsByClassName('place')];
-
-      function updatePlace(element, value) {
-        (value === 0) ? element.innerHTML = '' : element.innerHTML = value;
-      }
-
-      places.forEach((element) => {
-        updatePlace(element, 0);
-      });
-      gameStarted = false;
-      playingPlayer = playerOne;
-      gameResult = 0;
-      roundNumber = 0;
-      const decision = document.getElementById('result');
-      decision.innerHTML = '';
-      decision.classList.remove('drawn');
-      decision.classList.remove('winned');
-      document.getElementById('player').innerHTML = `Player ${playingPlayer}`;
-    } else {
-      // console.log('nothing to reset. no Game is going on')
-      // console.log('matrix is '+matrix)
-    }
-    console.clear();
-  });
-}
-
-function gameDecision(currentResult, roundNumber) {
-  // console.log(roundNumber)
-
-  const decision = document.getElementById('result');
-
-  if (currentResult.winned) {
-    console.log(`congratulations to player ${currentResult.winner}`);
-    console.log('End of the Game');
-    document.getElementById('player').innerHTML = '...';
-
-    decision.classList.add('winned');
-    decision.innerHTML = `Player ${currentResult.winner} Won !`;
-  } else if (roundNumber > size + 1) {
-    console.log('It is a Draw Game');
-    console.log('End of the Game');
-    // decision.classList.remove('winned')
-    decision.classList.add('drawn');
-    decision.innerHTML = 'It is a Drawn Game !';
-  }
-}
-function drawCanvas() {
-  // console.log('Drawing canvas')
-  const content = `
-        <div class="container mainGameCtn">
-        <div class="game">
-            <div class="place" id="p1"></div>
-            <div class="place" id="p2"></div>
-            <div class="place" id="p3"></div>
-            <div class="place" id="p4"></div>
-            <div class="place" id="p5"></div>
-            <div class="place" id="p6"></div>
-            <div class="place" id="p7"></div>
-            <div class="place" id="p8"></div>
-            <div class="place" id="p9"></div>
-        </div>
-    </div>
-    <div class="gameTools">
-        <span class="title">Games Tools</span>
-        
-        <div class="tools">
-            <span class="title result" id="result"></span>
-            <button id="resetGame">Start New Game</button>
-        </div>
-        <span class="title currentPlayer">Current player is  <span class="player" id="player">${playingPlayer}</span></span>
-    </div>
-    `;
-  gameZone.innerHTML = content;
-  setEventListener();
+function updatePlace(element, value) {
+  // if (value === 0) element.innerHTML = 'M'; element.innerHTML = value;
+  // console.log('updating...', value);
+  if (value !== 0) element.innerHTML = value; element.innerHTML = ' ';
 }
 
 function refreshCanvas() {
@@ -398,33 +266,18 @@ function refreshCanvas() {
   // console.log(matrix)
 }
 
-/**
- * a player proceed with his action event
- * @params {element} params
- *      a
- */
 function playEvent(element) {
   const id = element.getAttribute('id');
 
-  function play(coordinates) {
-    setMatrixValue(coordinates, playingPlayer);
+  function play() {
+    // setMatrixValue(coordinates, playingPlayer);
     refreshCanvas();
     if (playingPlayer === playerTwo) {
-      roundNumber++;
+      roundNumber += 1;
     }
     playingPlayer = (playingPlayer === playerOne) ? playerTwo : playerOne;
 
-    // Setting the player turn
     document.getElementById('player').innerHTML = `Player ${playingPlayer}`;
-    // console.log('round :'+roundNumber,'game result :'+gameResult.winned)
-
-    // if (gameResult.winned) {
-    //     console.log('a winner is settle. Game should stop')
-    // } else
-    // if (roundNumber > size ) {
-    //     console.log('No winner founded after tha max amount of rounds. Game should stop')
-    // }
-    // console.log(player)
   }
   // console.log(id)
   switch (id) {
@@ -488,3 +341,124 @@ function playEvent(element) {
       break;
   }
 }
+
+function gameDecision(currentResult, roundNumber) {
+  const decision = document.getElementById('result');
+
+  if (currentResult.winned) {
+    // console.log(`congratulations to player ${currentResult.winner}`);
+    // console.log('End of the Game');
+    document.getElementById('player').innerHTML = '...';
+
+    decision.classList.add('winned');
+    decision.innerHTML = `Player ${currentResult.winner} Won !`;
+  } else if (roundNumber > size + 1) {
+    // console.log('It is a Draw Game');
+    // console.log('End of the Game');
+    // decision.classList.remove('winned')
+    decision.classList.add('drawn');
+    decision.innerHTML = 'It is a Drawn Game !';
+  }
+}
+
+function setEventListener() {
+  const places = [...document.getElementsByClassName('place')];
+  places.forEach((element) => {
+    element.addEventListener('click', () => {
+      if (gameResult.winned) {
+        // console.log('yes');
+      } else if (roundNumber > size + 1) {
+        // console.log('no');
+      } else {
+        if (roundNumber === size + 1) roundNumber += 1;
+        if (!gameStarted) {
+          gameStarted = true;
+        }
+        playEvent(element);
+        // console.log('The Matrix now is : '+ matrix)
+
+        const result = checkGame(matrix);
+        for (let index = 0; index < result.length; index += 1) {
+          const element = result[index];
+          if (element.winned) {
+            gameResult = element;
+            break;
+          }
+        }
+
+        gameDecision(gameResult, roundNumber);
+      }
+    });
+  });
+
+  const resetBtn = document.getElementById('resetGame');
+  resetBtn.addEventListener('click', () => {
+    if (gameStarted) {
+      resetMatrix();
+
+      const places = [...document.getElementsByClassName('place')];
+
+      places.forEach((element) => {
+        updatePlace(element, 0);
+      });
+      gameStarted = false;
+      playingPlayer = playerOne;
+      gameResult = 0;
+      roundNumber = 0;
+      const decision = document.getElementById('result');
+      decision.innerHTML = '';
+      decision.classList.remove('drawn');
+      decision.classList.remove('winned');
+      document.getElementById('player').innerHTML = `Player ${playingPlayer}`;
+    } else {
+      // console.log('nothing to reset. no Game is going on')
+      // console.log('matrix is '+matrix)
+    }
+    // console.clear();
+  });
+}
+
+function drawCanvas() {
+  // console.log('Drawing canvas')
+  const content = `
+        <div class="container mainGameCtn">
+        <div class="game">
+            <div class="place" id="p1"></div>
+            <div class="place" id="p2"></div>
+            <div class="place" id="p3"></div>
+            <div class="place" id="p4"></div>
+            <div class="place" id="p5"></div>
+            <div class="place" id="p6"></div>
+            <div class="place" id="p7"></div>
+            <div class="place" id="p8"></div>
+            <div class="place" id="p9"></div>
+        </div>
+    </div>
+    <div class="gameTools">
+        <span class="title">Games Tools</span>
+        
+        <div class="tools">
+            <span class="title result" id="result"></span>
+            <button id="resetGame">Start New Game</button>
+        </div>
+        <span class="title currentPlayer">Current player is  <span class="player" id="player">${playingPlayer}</span></span>
+    </div>
+    `;
+  gameZone.innerHTML = content;
+  setEventListener();
+}
+
+/**
+ * a player proceed with his action event
+ * @params {element} params
+ *      a
+ */
+
+document.getElementById('p1').addEventListener('click', () => {
+  function goGame() {
+    gameZone = document.getElementById('mainTttGame');
+    drawCanvas();
+    refreshCanvas();
+  }
+  goGame();
+});
