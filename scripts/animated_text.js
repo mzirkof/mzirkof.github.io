c6431595c631107a2e672da356957bf1c57b2499
Celestin SOUOP <souop_007@yahoo.fr>
2024-05-18 09:09:52 -0700

    Update animated_text.js
    Fixing other linters errors

--- scripts/animated_text.js
+++ scripts/animated_text.js
@@ -7,15 +7,50 @@ const words = [
   'Cameroonian 🌍',
   'a Teacher 🏫',
 ];
-
-function animatedText(container, words = []) {
+let write = true;
+let index = 0;
+function animatedText(container, word) {
+  write = false;
   let j = 0;
-  [MyText] = [words[0]];
+  // [MyText] = [words[0]];
+  MyText = word;
   // MyText = 'software developer';
   const textItem = container;
   const mySpeed = 50;
   const d = 100;
   const cursor = '<u class="cursor">|</u>';
+
+  function goLeft(myText, limit = 0, speed = mySpeed, delay = d, random = false) {
+    let result = myText;
+    let i = myText.length;
+    const interval = setInterval(() => {
+      result = MyText.slice(0, i) + cursor;
+      textItem.innerHTML = result;
+      i -= 1;
+
+      if (i === limit - 1) {
+        clearInterval(interval);
+        setTimeout(() => {
+          if (random) {
+            i = 0;
+          } else {
+            j += 1;
+            if (j === words.length) j = 0;
+          }
+
+          MyText = words[j];
+          // goRight(MyText);
+          if (index >= words.length - 1) {
+            index = 0;
+          } else {
+            index += 1;
+          }
+          write = true;
+        }, delay);
+      }
+    }, speed / 5);
+  }
+
   function goRight(myText, limit = myText.length, speed = mySpeed, delay = d) {
     let result = '';
     let i = 0;
@@ -38,35 +73,13 @@ function animatedText(container, words = []) {
   }
 
   // goRight(MyText)
-
-  function goLeft(myText, limit = 0, speed = mySpeed, delay = d, random = false) {
-    let result = myText;
-    let i = myText.length;
-    const interval = setInterval(() => {
-      result = MyText.slice(0, i) + cursor;
-      textItem.innerHTML = result;
-      i -= 1;
-
-      if (i === limit - 1) {
-        clearInterval(interval);
-        setTimeout(() => {
-          if (random) {
-            i = 0;
-          } else {
-            j += 1;
-            if (j === words.length) j = 0;
-          }
-
-          MyText = words[j];
-          goRight(MyText);
-        }, delay);
-      }
-    }, speed);
-  }
-
   goRight(MyText);
 }
 
 // alert()
 const item = document.getElementById('animText');
-animatedText(item, words);
+setInterval(() => {
+  if (write) {
+    animatedText(item, words[index]);
+  }
+}, 1000);
